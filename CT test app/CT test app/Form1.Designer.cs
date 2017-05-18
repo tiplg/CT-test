143c0c7248134362ec23eebe82333247632db679
Tijmen De Jong <tijmen.dejong@hotmail.com>
2017-05-19 01:43:34 +0200

    Come on come on

--- CT test app/CT test app/Form1.Designer.cs
+++ CT test app/CT test app/Form1.Designer.cs
@@ -28,33 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.cboPorts = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.rotLeft = new System.Windows.Forms.Button();
+            this.rotCenter = new System.Windows.Forms.Button();
+            this.rotRight = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btnScan
             // 
-            this.button1.Location = new System.Drawing.Point(88, 87);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnScan.Location = new System.Drawing.Point(93, 10);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "Scan Ports";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 39);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 1;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(93, 39);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // cboPorts
+            // 
+            this.cboPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPorts.FormattingEnabled = true;
+            this.cboPorts.Location = new System.Drawing.Point(12, 12);
+            this.cboPorts.Name = "cboPorts";
+            this.cboPorts.Size = new System.Drawing.Size(75, 21);
+            this.cboPorts.TabIndex = 3;
+            // 
+            // rotLeft
+            // 
+            this.rotLeft.Location = new System.Drawing.Point(64, 102);
+            this.rotLeft.Name = "rotLeft";
+            this.rotLeft.Size = new System.Drawing.Size(23, 23);
+            this.rotLeft.TabIndex = 4;
+            this.rotLeft.Text = "<";
+            this.rotLeft.UseVisualStyleBackColor = true;
+            this.rotLeft.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rotLeft_MouseDown);
+            this.rotLeft.MouseUp += new System.Windows.Forms.MouseEventHandler(this.rotLeft_MouseUp);
+            // 
+            // rotCenter
+            // 
+            this.rotCenter.Location = new System.Drawing.Point(93, 102);
+            this.rotCenter.Name = "rotCenter";
+            this.rotCenter.Size = new System.Drawing.Size(23, 23);
+            this.rotCenter.TabIndex = 5;
+            this.rotCenter.UseVisualStyleBackColor = true;
+            // 
+            // rotRight
+            // 
+            this.rotRight.Location = new System.Drawing.Point(122, 102);
+            this.rotRight.Name = "rotRight";
+            this.rotRight.Size = new System.Drawing.Size(23, 23);
+            this.rotRight.TabIndex = 6;
+            this.rotRight.Text = ">";
+            this.rotRight.UseVisualStyleBackColor = true;
+            this.rotRight.MouseDown += new System.Windows.Forms.MouseEventHandler(this.rotRight_MouseDown);
+            this.rotRight.MouseUp += new System.Windows.Forms.MouseEventHandler(this.rotRight_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "rotatie";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(607, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rotRight);
+            this.Controls.Add(this.rotCenter);
+            this.Controls.Add(this.rotLeft);
+            this.Controls.Add(this.cboPorts);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnScan);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.ComboBox cboPorts;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button rotLeft;
+        private System.Windows.Forms.Button rotCenter;
+        private System.Windows.Forms.Button rotRight;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CT test app/CT test app/Form1.cs
+++ CT test app/CT test app/Form1.cs
@@ -7,14 +7,120 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace CT_test_app
 {
     public partial class Form1 : Form
     {
+        private Timer mainTimer;
+
+        int rotatie = 0;
+
         public Form1()
         {
             InitializeComponent();
+            InitTimer();
+        }
+
+        private void mainLoop(object sender, EventArgs e)
+        {
+            //Console.WriteLine(rotatie);
+            updateCT();
+        }
+
+        public void InitTimer()
+        {
+            mainTimer = new Timer();
+            mainTimer.Tick += new EventHandler(mainLoop);
+            mainTimer.Interval = 1000;
+            mainTimer.Start();
+        }
+
+        public void updateCT()
+        {
+            if (serialPort1.IsOpen)
+            {
+                byte[] outBuffer = new byte[3];
+                outBuffer[0] = 1;
+                outBuffer[1] = 1;
+                outBuffer[2] = (byte)rotatie;
+                serialPort1.Write(outBuffer, 0, 3);
+                Console.WriteLine(outBuffer[2].ToString());
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            cboPorts.Items.AddRange(ports);
+            btnDisconnect.Enabled = false;
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            cboPorts.Items.Clear();
+            string[] ports = SerialPort.GetPortNames();
+            cboPorts.Items.AddRange(ports);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            btnDisconnect.Enabled = true;
+            cboPorts.Enabled = false;
+            btnScan.Enabled = false;
+
+            try
+            {
+                serialPort1.PortName = cboPorts.Text;
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                btnConnect.Enabled = true;
+                btnDisconnect.Enabled = false;
+                cboPorts.Enabled = true;
+                MessageBox.Show(ex.Message, "Not so helpful error message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+            cboPorts.Enabled = true;
+            btnScan.Enabled = true;
+
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Not so helpful error message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void rotLeft_MouseDown(object sender, MouseEventArgs e)
+        {
+            rotatie = 1;
+        }
+
+        private void rotLeft_MouseUp(object sender, MouseEventArgs e)
+        {
+            rotatie = 0;
+        }
+
+        private void rotRight_MouseDown(object sender, MouseEventArgs e)
+        {
+            rotatie = 2;
+        }
+
+        private void rotRight_MouseUp(object sender, MouseEventArgs e)
+        {
+            rotatie = 0;
         }
     }
 }
